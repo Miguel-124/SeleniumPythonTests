f1079ae92647e5ad3e240565017b8a7322e0a319
michalgorecki <michalgorecki124@gmail.com>
2024-07-07 15:16:04 +0200

    Adding chow to run test in deffrent browser

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -4,6 +4,7 @@ from selenium import webdriver
 def driver():
     print("Creating chrome driver instance")
     driver = webdriver.Chrome()
+    #driver = webdriver.Firefox() #--> Uncomment this line to run the tests in Firefox
     yield driver
     print("Closing chrome driver instance")
     driver.quit()
