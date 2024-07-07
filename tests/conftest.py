d31a68556ed99cc6c362cc8241d09e59826f8f7a
michalgorecki <michalgorecki124@gmail.com>
2024-07-07 15:26:28 +0200

    Adding browser parameters in configuration and code

---
+++ tests/conftest.py
@@ -0,0 +1,18 @@
+import pytest
+from selenium import webdriver
+@pytest.fixture()
+def driver(request):
+    browser = request.config.getoption("--browser")
+    print(f"Creating {browser} driver instance")
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    else:
+        raise Exception(f"{browser} is not supported!")
+    yield driver
+    print(f"Closing {browser} driver instance")
+    driver.quit()
+
+def pytest_addoption(parser):
+    parser.addoption("--browser", action="store", default="chrome", help="Type in browser name e.g. chrome OR firefox")
