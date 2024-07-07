db49d27c274911bde30a9d663fa6cd00dae7135b
michalgorecki <michalgorecki124@gmail.com>
2024-07-08 00:13:53 +0200

    Adding test2 test_element_not_interactable_exception

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -11,7 +11,6 @@ Verify Row 2 input field is displayed
 Row 2 doesn’t appear immediately. This test will fail with org.openqa.selenium.NoSuchElementException without proper wait"""
 class TestException:
     @pytest.mark.exceptions
-    @pytest.mark.positive
     def test_no_such_element_exception(self, driver):
         ### Open page
         driver.get("https://practicetestautomation.com/practice-test-exceptions/")
@@ -23,5 +22,19 @@ class TestException:
         row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")))
         assert row_2_input_element.is_displayed(), "Row2 is not displayed"
 
+    @pytest.mark.exceptions
+    @pytest.mark.debug
+    def test_element_not_interactable_exception(self, driver):
+        ### Open page
+        driver.get("https://practicetestautomation.com/practice-test-exceptions/")
+
+        driver.find_element(By.ID, "add_btn").click()
+
+        wait = WebDriverWait(driver, 10)
+        row_2_input_element = wait.until(ec.visibility_of_element_located((By.XPATH, "//div[@id='row2']/input")))
+        row_2_input_element.send_keys("Sushi")
+
+        driver.find_element(By.XPATH, "//div[@id='row2']/button[@name='Save']").click()
+        assert driver.find_element(By.ID, "confirmation").text == "Row 2 was saved", "Row 2 was not saved"
 
 
