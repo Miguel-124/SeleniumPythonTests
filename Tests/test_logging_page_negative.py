eae0265e2571bec35c978e2186f58267c3c0db64
michalgorecki <michalgorecki124@gmail.com>
2024-07-07 14:38:41 +0200

    Adding negative test for incorrect user and incorrect password

--- Tests/test_logging_page_negative.py
+++ Tests/test_logging_page_negative.py
@@ -3,18 +3,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import pytest
 
-
-"""Test case 2: Negative username test
-Open page
-Type username incorrectUser into Username field
-Type password Password123 into Password field
-Push Submit button
-Verify error message is displayed
-Verify error message text is Your username is invalid!"""
 class TestNegativeScenerios:
+    """Test case 2: Negative username test
+    Open page
+    Type username incorrectUser into Username field
+    Type password Password123 into Password field
+    Push Submit button
+    Verify error message is displayed
+    Verify error message text is Your username is invalid!"""
     @pytest.mark.login
     @pytest.mark.negative
-    def test_negative_login(self):
+    def test_negative_username(self):
         ### Open page
         driver = webdriver.Chrome()
         driver.get("https://practicetestautomation.com/practice-test-login/")
@@ -38,3 +37,38 @@ class TestNegativeScenerios:
 
         ###Verify error message text is Your username is invalid!
         assert error_message_locator.text == "Your username is invalid!", "Error message is not correct"
+
+
+    """Test case 3: Negative password test
+    Open page
+    Type username student into Username field
+    Type password incorrectPassword into Password field
+    Push Submit button
+    Verify error message is displayed
+    Verify error message text is Your password is invalid!"""
+    @pytest.mark.login
+    @pytest.mark.negative
+    def test_negative_password(self):
+        ### Open page
+        driver = webdriver.Chrome()
+        driver.get("https://practicetestautomation.com/practice-test-login/")
+
+        ###Type username student into Username field
+        username_locator = driver.find_element(By.ID, "username")
+        username_locator.send_keys("student")
+
+        ###Type password incorrectPassword into Password field
+        password_locator = driver.find_element(By.NAME, "password")
+        password_locator.send_keys("incorrectPassword")
+
+        ###Push Submit button
+        subbmit_button_locator = driver.find_element(By.XPATH, "//button[@class='btn']")
+        subbmit_button_locator.click()
+        time.sleep(2)
+
+        ###Verify error message is displayed
+        error_message_locator = driver.find_element(By.ID, "error")
+        assert error_message_locator.is_displayed(), "Error message is not displayed"
+
+        ###Verify error message text is Your password is invalid!
+        assert error_message_locator.text == "Your password is invalid!", "Error message is not correct"
